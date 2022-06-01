652b95864fd56f1deee86203a01da5ed975da259
KingLud <cossu.luca@gmail.com>
2022-06-01 23:35:06 +0200

    check proxies and add seed description

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 
 include("simple_html_dom.php");
 
-//$html = file_get_html('https://eztv.re/shows/438104/obi-wan-kenobi&quality=1080');
-$html = file_get_html('https://eztv.re/shows/'.$_REQUEST['show']);
-//$show ="obi-wan-kenobi";
+
+$status = file_get_html('https://eztvstatus.com');
+
+foreach ($status->find('a') as $domain) {
+	if ($domain->class == 'domainLink') {
+		$available = @file_get_html($domain->href);
+		if ($available != false) {
+			$dom = $domain->href;
+			break;
+		}
+	}
+}
+
+// $html = file_get_html( $dom . '/shows/438104/obi-wan-kenobi&quality=1080');
+$html = file_get_html($dom . '/shows/' . $_REQUEST['show']);
 $show = $_REQUEST['show'];
 $q = $_REQUEST['quality'];
-//$q = 1080;
+// $show ="obi-wan-kenobi";
+// $q = 1080;
 
 header("content-type:text/xml");
 
@@ -15,33 +28,32 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE torrent PUBLIC "-//bitTorrent//DTD torrent 0.1//EN" "http://xmlns.ezrss.io/0.1/dtd/">
 <rss version="2.0">
 	<channel>
-		<title>ezRSS - ' . $show . ' '.  $q . 'p</title>
+		<title>ezRSS - ' . $show . ' ' .  $q . 'p</title>
 	<description>Custom RSS feed based off search filters.</description>';
 
 $found = array();
 
-foreach($html->find('a') as $element) {
-	if($element->class == 'magnet'){//download_1
+foreach ($html->find('a') as $element) {
+	if ($element->class == 'magnet') { //download_1
 		$title = $element->parent()->prev_sibling()->first_child()->title;
-		if(str_contains($title, $q.'p')){
-			preg_match('/S\d{2}E\d{2}/',$title, $matches);
+		if (str_contains($title, $q . 'p')) {
+			preg_match('/S\d{2}E\d{2}/', $title, $matches);
 			$ep = $matches[0];
-			
-			$seedText = $element->parent()->next_sibling()->next_sibling()->next_sibling()->first_child();
-			if(!is_null($seedText)){
-				$seeds = substr($seedText->__toString(),20);
-				$seeds = str_replace(",","", $seeds);
-				$seeds = intval($seeds);
-			}
-			else $seeds = 0;
 
-			if(array_key_exists($ep, $found)){
-				$e = array('title'=>$title, 'magnet'=>$element->href, 'seeds'=>$seeds);
-				$found[$ep][]= $e;
-			}else{
-				$found[$ep] = array(array('title'=>$title, 'magnet'=>$element->href, 'seeds'=>$seeds));
+			$seedText = $element->parent()->next_sibling()->next_sibling()->next_sibling()->first_child();
+			if (!is_null($seedText)) {
+				$seeds = substr($seedText->__toString(), 20);
+				$seeds = str_replace(",", "", $seeds);
+				$seeds = intval($seeds);
+			} else $seeds = 0;
+
+			if (array_key_exists($ep, $found)) {
+				$e = array('title' => $title, 'magnet' => $element->href, 'seeds' => $seeds);
+				$found[$ep][] = $e;
+			} else {
+				$found[$ep] = array(array('title' => $title, 'magnet' => $element->href, 'seeds' => $seeds));
 			}
-			
+
 			// echo "<item>";
 			// echo '<title><![CDATA['.$element->parent()->prev_sibling()->first_child()->title.']]></title>
 			// 	<link><![CDATA['.$element->href.']]></link>';
@@ -58,13 +70,15 @@ foreach ($found as $key => $value) {
 	$maxs = array_keys(array_map("findseed", $value), max(array_map("findseed", $value)))[0];
 	$element = $value[$maxs];
 	echo "<item>";
-	echo '<title><![CDATA['.$element["title"].']]></title>
-	<link><![CDATA['.$element["magnet"].']]></link>';
+	echo '<title>'.$element["title"].'</title>
+			<link>'.$element["magnet"].'</link>
+			<description>Seeds: '. $element['seeds'] . '</description>';
 	echo "</item>";
 }
-function findseed($v){
+function findseed($v)
+{
 	return $v['seeds'];
- }
+}
 
 echo '</channel>
 </rss>';
